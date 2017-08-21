e6776394d57d693f3dae59d8fcda50841910ac1e
Deshan Xiao <1254582845@qq.com>
2017-08-21 09:27:46 +0800

    添加希尔排序

---
+++ src/main/java/com/hust/xds/sort/ShellSort.java
@@ -0,0 +1,24 @@
+package com.hust.xds.sort;
+
+import com.hust.xds.util.Utils;
+
+/**
+ * 希尔排序java实现
+ *
+ * @author Deshan Xiao
+ */
+public class ShellSort {
+
+	public static void sort(int[] arr) {
+		int len = arr.length;
+
+		for (int grap = (len >> 1); grap > 0; grap = (grap >> 1)) {
+			for (int i = grap; i < len; i++) {
+				for (int j = i - grap; j >= 0 && arr[j] > arr[j + grap]; j -= grap) {
+					Utils.swap(arr, j, j + grap);
+				}
+			}
+		}
+	}
+
+}
